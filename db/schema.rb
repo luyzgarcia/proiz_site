f29ef4a52a5d65aaf20f41540fb2f21a31961fc6
proiz <luiz@proiz.com.br>
2014-05-05 09:29:23 -0300

    Alteracoes no cadastro de clientes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430125625) do
+ActiveRecord::Schema.define(version: 20140502191045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 20140430125625) do
     t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
+    t.integer  "status"
   end
 
 end
