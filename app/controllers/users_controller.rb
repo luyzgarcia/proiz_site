9681cf2cbc56ca47ec8b2ce26cf4e9a262d26456
Henrique <henrique@proiz.com.br>
2014-05-06 08:25:52 -0300

    Adicionado def show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,25 @@ class UsersController < ApplicationController
     end
   end
   
+  def show
+    @users = User.all
+  end
+  
   def new
     @user = User.new
     respond_to do |format|
-        format.js
+        format.js {render action: 'new'}
+        format.html {render action: 'new'}
     end
   end
   
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to painel_index_path, :notice => "Cadastrado"
+      respond_to do |format|
+        format.js { redirect_to users_show_path }
+      end      
+      
     else
       respond_to do |format|
         format.js {render action: 'new'} # resposta em formato JS para atualizar apenas a DIV= "detalhe_tabela"
