83292c49cbeb6e21c1fcbb765abad2b42098eb1e
Luiz Garcia <Luizgarcia@Mac-Pro-de-Luiz.local>
2014-07-24 12:00:01 -0700

    adicionado js de remarketing

--- app/mailers/especialidade_mailer.rb
+++
@@ -1,7 +0,0 @@
-class EspecialidadeMailer < ActionMailer::Base
-  default from: 'luiz@proiz.com.br'
-  
-  def email_teste
-    mail(to: 'luyzgarcia@gmail.com', subject: 'Email teste do site proiz')
-  end
-end
