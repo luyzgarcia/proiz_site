bc17264cb671d1f4b33b4d1b14c5376c8c4269d7
Luiz Garcia <luyzgarcia@gmail.com>
2014-08-25 15:11:07 -0700

    corrigido email de contato

--- app/mailers/proiz_mailer.rb
+++ app/mailers/proiz_mailer.rb
@@ -3,13 +3,13 @@ class ProizMailer < ActionMailer::Base
   
   def contato_especialidades(dados)
     @dados = dados
-    mail(to: 'atendimento@proiz.com', subject: 'Contato sobre Especialidades')
-    mail(to: 'luyzgarcia@gmail.com', subject: 'Contato sobre Especialidades')
+    mail(to: 'atendimento@proiz.com.br', subject: 'Contato sobre Especialidades')
+    #mail(to: 'luyzgarcia@gmail.com', subject: 'Contato sobre Especialidades')
   end
   
   def contato(dados)
     @dados = dados
-    mail(to: 'atendimento@proiz.com', subject: 'Proiz - Contato')
-    mail(to: 'luyzgarcia@gmail.com', subject: 'Proiz - Contato')
+    mail(to: 'atendimento@proiz.com.br', subject: 'Proiz - Contato')
+    #mail(to: 'luyzgarcia@gmail.com', subject: 'Proiz - Contato')
   end
 end
